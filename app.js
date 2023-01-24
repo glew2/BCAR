5523ebf87d1812e70ea420c4a3b7e99ce6c8fbba
Grant Lewison <60186503+glew2@users.noreply.github.com>
2023-01-23 22:14:30 -0500

    Routing issues resolved and Database connection established!

--- app.js
+++ app.js
@@ -1,35 +1,51 @@
 const express = require('express');
 const auth0 = require('auth0');
 const path = require('path');
+const mysql = require('mysql');
 
 const app = express();
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.redirect('/login');
 })
 
 app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '/login.html'));
+  res.render('login');
 });
 
-app.get('/logout', (req, res) => {
-     res.sendFile(path.join(__dirname, '/login.html'));
-});
+// app.get('/logout', (req, res) => {
+//      res.sendFile(path.join(__dirname, '/login'));
+// });
 
 app.get('/callback', (req, res) => {
   res.redirect('/landing');
 });
 
 app.get('/landing', (req, res) => {
-  res.sendFile(path.join(__dirname, '/landing.html'));
+  res.render('landing');
 });
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/sample_data', (req, res) => {
-  res.sendFile(__dirname, '/sample_data.ejs');
+const pool = mysql.createPool({
+  host: 'soccerdb.calingaiy4id.us-east-2.rds.amazonaws.com',
+  user: 'ale_lew_mus',
+  password: 'QS7FhtcdeLbm',
+  database: 'capstone_2223_bcareshub'
+});
+
+app.get('/data_display', (req, res) => {
+  pool.query('SELECT * FROM projects', (error, results) => {
+    if (error) {
+      return res.send(error.message);
+    }
+    res.render('data_display', { projects: results });
+  });
 });
 
 app.listen(3000, () => {
